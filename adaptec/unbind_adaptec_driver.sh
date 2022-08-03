7d04da678e876b78f2df1a1d82398b1e7747fb33
Xianglong Meng <xlmeng1988@qq.com>
2022-08-03 15:49:53 +0800

    add -x option for bash script

--- adaptec/unbind_adaptec_driver.sh
+++ adaptec/unbind_adaptec_driver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 vendor_id=9005
 device_id=$(lspci -d ${vendor_id}: -n | cut -d' ' -f3 | cut -d: -f2)
 slot=$(lspci -d ${vendor_id}: -n | cut -d' ' -f1)
@@ -8,7 +8,7 @@ echo "Current SCSI devices: "
 lsscsi
 
 # unbind driver and device
-echo "unbind current device:0000:${slot} with driver"
+echo "unbind current device: 0000:${slot} with driver"
 echo 0000:${slot} > /sys/bus/pci/devices/0000:${slot}/driver/unbind
 
 echo "Now, Current SCSI devices: "

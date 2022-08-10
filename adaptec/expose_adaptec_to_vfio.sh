086b76ae100dae002f3195cc7702b57acdea88db
Xianglong Meng <xlmeng1988@qq.com>
2022-08-10 18:27:25 +0800

    update scripts

--- adaptec/expose_adaptec_to_vfio.sh
+++ adaptec/expose_adaptec_to_vfio.sh
@@ -10,9 +10,6 @@ echo ${vendor_id} ${device_id} > /sys/bus/pci/drivers/vfio-pci/new_id
 
 ls /dev/vfio
 
-echo "Remove aacriad driver..."
-rmmod aacraid
-
 reset_method=/sys/bus/pci/devices/0000:${slot}/reset_method
 echo "Current reset_method: $(cat ${reset_method})"
 echo "Remove FLR reset_method"
--- adaptec/unbind_adaptec_driver.sh
+++ adaptec/unbind_adaptec_driver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+
 vendor_id=9005
 device_id=$(lspci -d ${vendor_id}: -n | cut -d' ' -f3 | cut -d: -f2)
 slot=$(lspci -d ${vendor_id}: -n | cut -d' ' -f1)
@@ -7,9 +8,12 @@ slot=$(lspci -d ${vendor_id}: -n | cut -d' ' -f1)
 echo "Current SCSI devices: "
 lsscsi
 
+echo "Remove aacriad driver..."
+rmmod aacraid
+
 # unbind driver and device
-echo "unbind current device: 0000:${slot} with driver"
-echo 0000:${slot} > /sys/bus/pci/devices/0000:${slot}/driver/unbind
+#echo "unbind current device: 0000:${slot} with driver"
+#echo 0000:${slot} > /sys/bus/pci/devices/0000:${slot}/driver/unbind
 
 echo "Now, Current SCSI devices: "
 lsscsi
